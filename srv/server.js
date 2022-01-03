1b890339b72ca8c4e116a4696645c2e02b551914
vin1974 <52678284+vin1974@users.noreply.github.com>
2022-01-03 17:15:28 -0500

    Update server.js

--- srv/server.js
+++ srv/server.js
@@ -2,7 +2,6 @@ const cds = require('@sap/cds');
 const proxy = require('@sap/cds-odata-v2-adapter-proxy');
 
 cds.on('bootstrap', app => {
-    console.log('hi there, Eppie...');
     app.use(proxy())
 });
 
